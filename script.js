5d33ae218ab5be38578dc9d9131637d8561ed2f2
Luiz Faria <luizguidefaria@gmail.com>
2024-03-02 20:34:11 -0300

    Tarefa 1 Concluída

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z]/g, ""); 
+  let right = cleanStr.length - 1;
+
+  for (let i = 0; i < cleanStr.length / 2; i++) {
+    if (cleanStr[i] !== cleanStr[right - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
